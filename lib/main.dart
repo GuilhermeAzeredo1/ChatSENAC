80ecd7c4a45fb2e0395e75a231326203add8de7d
Dhyonatan <helmerdhy@gmail.com>
2026-07-20 21:43:16 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(
+      Center(
+        child: Text(
+          "Olá, Mundo!",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+    );
+  }
